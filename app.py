6251f6694b02a1f92a4462d2be6ffee6b8923cc5
hitto <68594072+hitto-hub@users.noreply.github.com>
2024-03-07 14:31:42 +0900

    日本時間変更 datetime->string

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import Flask, g, request
 from flask_sqlalchemy import SQLAlchemy
-import pytz
+from zoneinfo import ZoneInfo
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -11,7 +11,7 @@ db = SQLAlchemy(app)
 class Data(db.Model):
     __tablename__ = 'data'
     id = db.Column(db.Integer, unique=True, primary_key=True)
-    timestamp = db.Column(db.DateTime, nullable=False)
+    timestamp = db.Column(db.String, nullable=False)
     val = db.Column(db.Integer, nullable=False)
     def __repr__(self):
         return '<Data %r>' % self.val
@@ -19,7 +19,7 @@ class Data(db.Model):
 class NoticeData(db.Model):
     __tablename__ = 'notice_data'
     id = db.Column(db.Integer, unique=True, primary_key=True)
-    timestamp = db.Column(db.DateTime, nullable=False)
+    timestamp = db.Column(db.String, nullable=False)
     notice = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
@@ -28,7 +28,7 @@ class NoticeData(db.Model):
 class FlagData(db.Model):
     __tablename__ = 'flag_data'
     id = db.Column(db.Integer, unique=True, primary_key=True)
-    timestamp = db.Column(db.DateTime, nullable=False)
+    timestamp = db.Column(db.String, nullable=False)
     flag = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
@@ -124,8 +124,9 @@ def get_val():
 @app.route('/api/val', methods=['POST'])
 def post_val():
     data = request.json["val"] #POSTメソッド のデータを取得
-    timestamp = datetime.now(pytz.timezone("Asia/Tokyo"))
-    cre = Data(val = data, timestamp = timestamp)
+    timestamp = datetime.now(ZoneInfo("Asia/Tokyo"))
+    timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    cre = Data(val = data, timestamp = timestamp_str)
     db.session.add(cre)
     db.session.commit()
     return 'post val ok\n '
@@ -150,8 +151,9 @@ def get_notice():
 @app.route('/api/notice', methods=['POST'])
 def post_notice():
     data = request.json["notice"] #POSTメソッド のデータを取得
-    timestamp = datetime.now(pytz.timezone("Asia/Tokyo"))
-    cre = NoticeData(notice = data, timestamp = timestamp)
+    timestamp = datetime.now(ZoneInfo("Asia/Tokyo"))
+    timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    cre = NoticeData(notice = data, timestamp = timestamp_str)
     db.session.add(cre)
     db.session.commit()
     return 'post notice ok\n '
@@ -210,8 +212,9 @@ def get_flag_count():
 @app.route('/api/flag', methods=['POST'])
 def post_flag():
     data = request.json["flag"] #POSTメソッド のデータを取得
-    timestamp = datetime.now(pytz.timezone("Asia/Tokyo"))
-    cre = FlagData(flag = data, timestamp = timestamp)
+    timestamp = datetime.now(ZoneInfo("Asia/Tokyo"))
+    timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    cre = FlagData(flag = data, timestamp = timestamp_str)
     db.session.add(cre)
     db.session.commit()
     return {
